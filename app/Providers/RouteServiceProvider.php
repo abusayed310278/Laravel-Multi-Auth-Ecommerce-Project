b9ebc1695f3c01024f4d8742a8fd3162b2ac3702
abu <abusayed310278@gmail.com>
2024-12-11 20:59:26 +0600

    more customize authentication for vendor and admin

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -33,6 +33,17 @@ class RouteServiceProvider extends ServiceProvider
 
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
+
+            Route::middleware(['web','auth','role:vendor'])
+                ->prefix('admin')
+                ->as('admin.')
+                ->group(base_path('routes/vendor.php'));
+
+            Route::middleware(['web','auth','role:vendor'])
+                ->prefix('vendor')
+                ->as('vendor.')
+                ->group(base_path('routes/admin.php'));
+
         });
     }
 
