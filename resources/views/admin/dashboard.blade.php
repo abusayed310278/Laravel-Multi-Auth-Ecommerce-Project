07a126ec96ab40f94c9817df58b125fec8ac51b9
abu <abusayed310278@gmail.com>
2024-12-11 22:39:30 +0600

    admin template setup

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,7 +1,10 @@
+<!--<editor-fold desc="Description">-->
+<!--region Description-->
+<!--<editor-fold desc="Description">-->
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8" />
+    <meta charset="UTF-8"/>
     <meta
         content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no"
         name="viewport"
@@ -9,40 +12,40 @@
     <title>General Dashboard &mdash; Stisla</title>
 
     <!-- General CSS Files -->
-    <link
-        rel="stylesheet"
-        href="assets/modules/bootstrap/css/bootstrap.min.css"
-    />
-    <link rel="stylesheet" href="assets/modules/fontawesome/css/all.min.css" />
+    <link rel="stylesheet" href="{{asset('backend/assets/modules/bootstrap/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('backend/assets/modules/fontawesome/css/all.min.css')}}">
 
     <!-- CSS Libraries -->
-    <link rel="stylesheet" href="assets/modules/jqvmap/dist/jqvmap.min.css" />
-    <link
-        rel="stylesheet"
-        href="assets/modules/weather-icon/css/weather-icons.min.css"
-    />
-    <link
-        rel="stylesheet"
-        href="assets/modules/weather-icon/css/weather-icons-wind.min.css"
-    />
-    <link
-        rel="stylesheet"
-        href="assets/modules/summernote/summernote-bs4.css"
-    />
+    <link rel="stylesheet" href="{{asset('backend/assets/modules/jqvmap/dist/jqvmap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('backend/assets/modules/weather-icon/css/weather-icons.min.css')}}">
+    <link rel="stylesheet" href="{{asset('backend/assets/modules/weather-icon/css/weather-icons-wind.min.css')}}">
+    <link rel="stylesheet" href="{{asset('backend/assets/modules/summernote/summernote-bs4.css')}}">
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+    <link rel="stylesheet" href="//cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap5.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css"/>
+    <link rel="stylesheet" href="{{asset('backend/assets/css/bootstrap-iconpicker.min.css')}}">
+    <link rel="stylesheet" href="{{asset('backend/assets/modules/bootstrap-daterangepicker/daterangepicker.css')}}">
+    <link rel="stylesheet" href="{{asset('backend/assets/modules/select2/dist/css/select2.min.css')}}">
 
     <!-- Template CSS -->
-    <link rel="stylesheet" href="assets/css/style.css" />
-    <link rel="stylesheet" href="assets/css/components.css" />
-    <!-- Start GA -->
+    <link rel="stylesheet" href="{{asset('backend/assets/css/style.css')}}">
+    <link rel="stylesheet" href="{{asset('backend/assets/css/components.css')}}">
+
+
+
+
     <script
         async
         src="https://www.googletagmanager.com/gtag/js?id=UA-94034622-3"
     ></script>
     <script>
         window.dataLayer = window.dataLayer || [];
+
         function gtag() {
             dataLayer.push(arguments);
         }
+
         gtag("js", new Date());
 
         gtag("config", "UA-94034622-3");
@@ -50,9 +53,12 @@
     <!-- /END GA -->
 </head>
 
+<!--<editor-fold desc="Description">-->
 <body>
 <div id="app">
+
     <div class="main-wrapper main-wrapper-1">
+
         <div class="navbar-bg"></div>
 
         <nav class="navbar navbar-expand-lg main-navbar">
@@ -343,7 +349,8 @@
                         />
                         <div class="d-sm-none d-lg-inline-block">
                             Hi, Ujang Maman
-                        </div></a
+                        </div>
+                    </a
                     >
                     <div class="dropdown-menu dropdown-menu-right">
                         <div class="dropdown-title">Logged in 5 min ago</div>
@@ -586,45 +593,62 @@
                 </ul>
 
 
-
             </aside>
         </div>
-
 
 
         <footer class="main-footer">
             <div class="footer-left">
                 Copyright &copy; 2018
                 <div class="bullet"></div>
-                Design By <a href="https://nauval.in/">Muhamad Nauval Azhar</a>
+                Design By <a href="#">Abu Sayed</a>
             </div>
             <div class="footer-right"></div>
         </footer>
+
+
     </div>
+
+
+
 </div>
 
 <!-- General JS Scripts -->
-<script src="assets/modules/jquery.min.js"></script>
-<script src="assets/modules/popper.js"></script>
-<script src="assets/modules/tooltip.js"></script>
-<script src="assets/modules/bootstrap/js/bootstrap.min.js"></script>
-<script src="assets/modules/nicescroll/jquery.nicescroll.min.js"></script>
-<script src="assets/modules/moment.min.js"></script>
-<script src="assets/js/stisla.js"></script>
+<script src="{{asset('backend/assets/modules/jquery.min.js')}}"></script>
+<script src="{{asset('backend/assets/modules/popper.js')}}"></script>
+<script src="{{asset('backend/assets/modules/tooltip.js')}}"></script>
+<script src="{{asset('backend/assets/modules/bootstrap/js/bootstrap.min.js')}}"></script>
+<script src="{{asset('backend/assets/modules/nicescroll/jquery.nicescroll.min.js')}}"></script>
+<script src="{{asset('backend/assets/modules/moment.min.js')}}"></script>
+<script src="{{asset('backend/assets/js/stisla.js')}}"></script>
 
 <!-- JS Libraies -->
-<script src="assets/modules/simple-weather/jquery.simpleWeather.min.js"></script>
-<script src="assets/modules/chart.min.js"></script>
-<script src="assets/modules/jqvmap/dist/jquery.vmap.min.js"></script>
-<script src="assets/modules/jqvmap/dist/maps/jquery.vmap.world.js"></script>
-<script src="assets/modules/summernote/summernote-bs4.js"></script>
-<script src="assets/modules/chocolat/dist/js/jquery.chocolat.min.js"></script>
+<script src="{{asset('backend/assets/modules/simple-weather/jquery.simpleWeather.min.js')}}"></script>
+{{-- <script src="{{asset('backend/assets/modules/chart.min.js')}}"></script> --}}
+<script src="{{asset('backend/assets/modules/jqvmap/dist/jquery.vmap.min.js')}}"></script>
+<script src="{{asset('backend/assets/modules/jqvmap/dist/maps/jquery.vmap.world.js')}}"></script>
+<script src="{{asset('backend/assets/modules/summernote/summernote-bs4.js')}}"></script>
+<script src="{{asset('backend/assets/modules/chocolat/dist/js/jquery.chocolat.min.js')}}"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+<script src="//cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap5.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<script src="{{asset('backend/assets/js/bootstrap-iconpicker.bundle.min.js')}}"></script>
+<script src="{{asset('backend/assets/modules/bootstrap-daterangepicker/daterangepicker.js')}}"></script>
+<script src="{{asset('backend/assets/modules/select2/dist/js/select2.full.min.js')}}"></script>
 
 <!-- Page Specific JS File -->
-<script src="assets/js/page/index-0.js"></script>
+{{-- <script src="{{asset('backend/assets/js/page/index-0.js')}}"></script> --}}
 
 <!-- Template JS File -->
-<script src="assets/js/scripts.js"></script>
-<script src="assets/js/custom.js"></script>
+<script src="{{asset('backend/assets/js/scripts.js')}}"></script>
+<script src="{{asset('backend/assets/js/custom.js')}}"></script>
+
+
+
 </body>
+<!--</editor-fold>-->
 </html>
+<!--</editor-fold>-->
+<!--endregion-->
+<!--</editor-fold>-->
