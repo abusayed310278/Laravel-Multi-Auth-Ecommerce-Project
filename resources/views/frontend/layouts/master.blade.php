a1d177b68e699ccc42bfaaf69caa39207b890138
abu <abusayed310278@gmail.com>
2024-12-12 19:20:37 +0600

    frontend login and register authentication and toastr notification setup

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -25,6 +25,7 @@
 
     <link rel="stylesheet" href="{{ asset('frontend/css/style.css')}}">
     <link rel="stylesheet" href="{{ asset('frontend/css/responsive.css')}}">
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
     <!-- <link rel="stylesheet" href="css/rtl.css"> -->
 </head>
 
@@ -151,6 +152,17 @@
 
 <!--main/custom js-->
 <script src="{{ asset('frontend/js/main.js')}}"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+
+<script>
+    @if ($errors->any())
+    @foreach ($errors->all() as $error)
+    toastr.error("{{$error}}")
+    @endforeach
+    @endif
+</script>
+
+
 </body>
 
 </html>
