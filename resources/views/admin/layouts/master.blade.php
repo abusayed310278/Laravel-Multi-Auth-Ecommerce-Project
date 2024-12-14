30a6361c0d7091d400c42b793f7c3928fc797e91
abu <abusayed310278@gmail.com>
2024-12-14 20:01:29 +0600

    protecting category relation in admin panel

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -62,9 +62,9 @@
 
         <footer class="main-footer">
             <div class="footer-left">
-                Copyright &copy; 2018
+                Copyright &copy; {{ date('Y') }}
                 <div class="bullet"></div>
-                Design By <a href="https://nauval.in/">Muhamad Nauval Azhar</a>
+                Design By <a href="#">Abu Sayed</a>
             </div>
             <div class="footer-right">
 
