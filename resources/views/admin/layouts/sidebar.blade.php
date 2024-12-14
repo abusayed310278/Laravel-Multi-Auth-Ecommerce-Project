5cba5829943b7b902009a6df1f696ebe91a9ad5c
abu <abusayed310278@gmail.com>
2024-12-15 00:27:32 +0600

    brand index and change status , datatable table create in admin panel

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -33,8 +33,7 @@
                     <li class="{{ setActive(['admin.sub-category.*']) }}"><a class="nav-link"
                                                                              href="{{ route('admin.sub-category.index') }}">SubCategory</a>
                     </li>
-                    <li class="{{ setActive(['admin.child-category.*']) }}"><a class="nav-link"
-                                                                               href="{{ route('admin.child-category.index') }}">ChildCategory</a>
+                    <li class="{{ setActive(['admin.child-category.*']) }}"><a class="nav-link" href="{{ route('admin.child-category.index') }}">ChildCategory</a>
                     </li>
                 </ul>
             </li>
@@ -48,8 +47,7 @@
             }}">
                 <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Manage Website</span></a>
                 <ul class="dropdown-menu">
-                    <li class="{{ setActive(['admin.slider.*']) }}"><a class="nav-link"
-                                                                       href="{{ route('admin.slider.index') }}">Slider</a></li>
+                    <li class="{{ setActive(['admin.slider.*']) }}"><a class="nav-link" href="{{ route('admin.slider.index') }}">Slider</a></li>
 
 
 
@@ -58,16 +56,28 @@
 
 
 
-{{--            <li class="dropdown">--}}
-{{--                <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Layout</span></a>--}}
-{{--                <ul class="dropdown-menu">--}}
-{{--                    <li><a class="nav-link" href="layout-default.html">Default Layout</a></li>--}}
-{{--                    <li><a class="nav-link" href="layout-transparent.html">Transparent Sidebar</a></li>--}}
-{{--                    <li><a class="nav-link" href="layout-top-navigation.html">Top Navigation</a></li>--}}
-{{--                </ul>--}}
-{{--            </li>--}}
+            <li class="dropdown {{ setActive([ 'admin.brand.*']) }}">
+                <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Manage Products</span></a>
+                <ul class="dropdown-menu">
+                    <li class="{{ setActive(['admin.brand.*']) }}"><a class="nav-link" href="{{ route('admin.brand.index') }}">Brands</a></li>
 
-{{--            <li><a class="nav-link" href="blank.html"><i class="far fa-square"></i> <span>Blank Page</span></a></li>--}}
+                </ul>
+            </li>
+
+            <li><a class="nav-link" href="blank.html"><i class="far fa-square"></i> <span>Blank Page</span></a></li>
+
+
+
+            {{--            <li class="dropdown">--}}
+            {{--                <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Layout</span></a>--}}
+            {{--                <ul class="dropdown-menu">--}}
+            {{--                    <li><a class="nav-link" href="layout-default.html">Default Layout</a></li>--}}
+            {{--                    <li><a class="nav-link" href="layout-transparent.html">Transparent Sidebar</a></li>--}}
+            {{--                    <li><a class="nav-link" href="layout-top-navigation.html">Top Navigation</a></li>--}}
+            {{--                </ul>--}}
+            {{--            </li>--}}
+
+            {{--            <li><a class="nav-link" href="blank.html"><i class="far fa-square"></i> <span>Blank Page</span></a></li>--}}
 
         </ul>
 
