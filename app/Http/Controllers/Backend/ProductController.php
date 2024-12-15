423e8289c6c0798b8772019a13d1305126722c5f
abu <abusayed310278@gmail.com>
2024-12-15 13:50:28 +0600

    product index,create status code in admin panel

---
+++ app/Http/Controllers/Backend/ProductController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\DataTables\ProductDataTable;
+use App\Http\Controllers\Controller;
+use App\Models\Brand;
+use App\Models\Category;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(ProductDataTable $dataTable)
+    {
+        return $dataTable->render('admin.product.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        $categories = Category::all();
+        $brands = Brand::all();
+        return view('admin.product.create', compact('categories', 'brands'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+
+    public function changeStatus(Request $request)
+    {
+        $product = Product::findOrFail($request->id);
+        $product->status = $request->status == 'true' ? 1 : 0;
+        $product->save();
+
+        return response(['message' => 'Status has been updated!']);
+    }
+
+
+}
