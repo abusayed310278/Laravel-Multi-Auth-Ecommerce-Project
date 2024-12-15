fc2d31a0dcf2332c4c8e734f5da606a638eead6b
abu <abusayed310278@gmail.com>
2024-12-15 11:42:34 +0600

    vendor banner update and AdminProfileSeeder in

--- routes/admin.php
+++ routes/admin.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Backend\AdminController;
 use App\Http\Controllers\Backend\BrandController;
+use App\Http\Controllers\Backend\AdminVendorProfileController;
 use App\Http\Controllers\Backend\CategoryController;
 use App\Http\Controllers\Backend\ChildCategoryController;
 use App\Http\Controllers\Backend\ProfileController;
@@ -36,3 +37,6 @@ Route::resource('child-category', ChildCategoryController::class);
 /** Brand routes */
 Route::put('brand/change-status', [BrandController::class, 'changeStatus'])->name('brand.change-status');
 Route::resource('brand', BrandController::class);
+
+/** Vendor Profile routes */
+Route::resource('vendor-profile', AdminVendorProfileController::class);
